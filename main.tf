637a215eb816e04da55a02b084664d6b1f95ff50
Rajendra <rajendrasinghdodiya394@gmail.com>
2026-09-07 14:58:12 +0530

    Correct folder structure and modified files

--- main.tf
+++ main.tf
@@ -1,33 +1,53 @@
-provider "aws" {
-    region = "eu-north-1"
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_config.cidr_block
+  tags = {
+    Name = var.vpc_config.name
+  }
 }
 
-module "vpc" {
-    source = "./module/vpc"
+resource "aws_subnet" "main" {
+  vpc_id   = aws_vpc.main.id
+  for_each = var.subnet_config #key={cidr, az} each.key each.value 
 
-    vpc_config = {
-        vpc_name = "my-test-vpc"
-        cidr_block = "10.0.0.0/16"
-    }
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.az
 
-    subnet_config = {
-        public_subnet-1 = {
-            cidr_block = "10.0.0.0/24"
-            az = "eu-north-1a"
-            public = true
-        }
+  tags = {
+    Name = each.key
+  }
+}
 
-        public_subnet-2 = {
-            cidr_block = "10.0.2.0/24"
-            az = "eu-north-1a"
-            public = true
-        }
+locals {
+  public_subnet = {
+    #key={} if public is true in subnet_config
+    for key, config in var.subnet_config : key => config if config.public
+  }
+  private_subnet = {
+    #key={} if public is true in subnet_config
+    for key, config in var.subnet_config : key => config if !config.public
+  }
+}
 
-        private_subnet = {
-            cidr_block = "10.0.1.0/24"
-            az = "eu-north-1b"
-            ## if not mention public , so by default private
-        }
-        
-    }
+#Internete Gateway, if there is alteast one public subnet
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  count  = length(local.public_subnet) > 0 ? 1 : 0
+}
+
+#Routing table
+resource "aws_route_table" "main" {
+  count  = length(local.public_subnet) > 0 ? 1 : 0
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main[0].id
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  for_each = local.public_subnet #public_subnet={} private_subnet={}
+
+  subnet_id      = aws_subnet.main[each.key].id
+  route_table_id = aws_route_table.main[0].id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,30 @@
-output "vpc" {
-    value = module.vpc.vpc_id
-  
+#VPC
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "public_subnet" {
-    value = module.vpc.public_subnets
-  
+locals {
+  #To format the subnet IDs which may be multiples in format of subnet_name = {id=, az=}
+  public_subnet_output = {
+    for key, config in local.public_subnet : key => {
+      subnet_id = aws_subnet.main[key].id
+      az        = aws_subnet.main[key].availability_zone
+    }
+  }
+
+  private_subnet_output = {
+    for key, config in local.private_subnet : key => {
+      subnet_id = aws_subnet.main[key].id
+      az        = aws_subnet.main[key].availability_zone
+    }
+  }
 }
 
-output "private_subnet" {
-    value = module.vpc.private_subnets
-  
+#Subnets Details
+output "public_subnets" {
+  value = local.public_subnet_output
+}
+
+output "private_subnets" {
+  value = local.private_subnet_output
 }
